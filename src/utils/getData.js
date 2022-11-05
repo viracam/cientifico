5d0cd9c24a47380435d9d9480bb0ec6c62d6a623
Camilo Viracachá <viracam@gmail.com>
2022-11-04 21:40:39 -0500

    pendiente get data

---
+++ src/utils/getData.js
@@ -0,0 +1,8 @@
+const API = 'https://rickandmortyapi.com/api/character/'
+
+const getData = async(id) =>{
+    const apiURL = id ? `${API}${id}` : API;
+    try{
+        const response = await fetch;
+    }
+}
