3313c388319a90d77363fdbd961aa2abd00fae83
Camilo Viracachá <viracam@gmail.com>
2022-11-10 17:10:33 -0500

    ultima version para traer la informacion

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -4,6 +4,8 @@ const getData = async(id) =>{
     const apiURL = id ? `${API}${id}` : API;
     try{
         const response = await fetch(apiURL);
+        const data = await response.json;
+        return data;
     }catch (error) {
         console.log('Fetch Error', error);
       };
