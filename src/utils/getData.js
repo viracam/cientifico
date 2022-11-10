a65cef2849b0c98dbe55352ad8c66dba9a9b30a2
Camilo Viracachá <viracam@gmail.com>
2022-11-10 13:53:54 -0500

    uso correcto del get data con catch y fetch lleva como paramentro apiURL

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,6 +3,9 @@ const API = 'https://rickandmortyapi.com/api/character/'
 const getData = async(id) =>{
     const apiURL = id ? `${API}${id}` : API;
     try{
-        const response = await fetch;
-    }
-}
+        const response = await fetch(apiURL);
+    }catch (error) {
+        console.log('Fetch Error', error);
+      };
+};
+export default getData;
