bf453822a671c3534c921ca36986d8d8d7096117
Camilo Viracachá <viracam@gmail.com>
2022-11-11 12:58:22 -0500

    cambio

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path =require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports ={
     entry: './src/index.js',
@@ -28,6 +28,15 @@ module.exports ={
                 filename: './index.html'
             }
 
+        ),
+        new CopyWebpackPlugin (
+            {
+                patterns: [{
+                    from: './src/styles/styles.css',
+                    to : '',
+                }
+                ]
+            }
         )
     ]
 
